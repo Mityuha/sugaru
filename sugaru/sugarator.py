85bb9e5ec9dd6beeff74447e4df9483a72e6d0b4
mityuha <mit.makaroff@gmail.com>
2023-04-29 18:05:33 +0200

    one more test

--- sugaru/sugarator.py
+++ sugaru/sugarator.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-from typing import List
+from typing import Dict, List, Optional
 
-from .interfaces import FinalFileWriter, PluginLoader, PluginNamesFetcher, SugarFileLoader
+from .interfaces import FinalFileWriter, Plugin, PluginLoader, PluginNamesFetcher, SugarFileLoader
 from .logging import logger
 
 
@@ -26,3 +26,20 @@ def sugarate(
         return
 
     logger.debug(f"Plugin list specified/loaded: {plugin_name_list}")
+
+    plugin_name: str
+    plugins: Dict[str, Plugin] = {}
+    for plugin_name in plugin_name_list:
+        plugin: Optional[Plugin] = plugin_loader(
+            plugin_name,
+            path_candidates=[],
+        )
+        if not plugin:
+            logger.warning(f"Unable to load plugin '{plugin_name}', skip")
+            continue
+
+        plugins[plugin_name] = plugin
+
+    if not plugins:
+        logger.warning("No one plugin was loaded, returning...")
+        return
--- tests/sugarator/test_sugarator.py
+++ tests/sugarator/test_sugarator.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 from sugaru import sugarate
 from . import SugarateArgs
@@ -13,5 +13,16 @@ def test_sugarate_no_plugins_to_fetch(sugarate_args: SugarateArgs, mocker: Any) 
 
     assert fetcher.mock_calls == [mocker.call()]
     assert sugarate_args["plugin_loader"].mock_calls == []  # type: ignore
+    assert sugarate_args["sugar_file_loader"].mock_calls == []  # type: ignore
+    assert sugarate_args["final_file_writer"].mock_calls == []  # type: ignore
+
+
+def test_sugarate_plugins_not_found(sugarate_args: SugarateArgs, mocker: Any) -> None:
+    plugins: List[str] = sugarate_args["plugin_name_list"]
+    sugarate_args["plugin_loader"].configure_mock(side_effect=[None for _ in range(len(plugins))])  # type: ignore
+    sugarate(**sugarate_args)
+
+    assert sugarate_args["plugin_names_fetcher"].mock_calls == []  # type: ignore
+    assert sugarate_args["plugin_loader"].mock_calls == [mocker.call(pname, path_candidates=mocker.ANY) for pname in plugins]  # type: ignore
     assert sugarate_args["sugar_file_loader"].mock_calls == []  # type: ignore
     assert sugarate_args["final_file_writer"].mock_calls == []  # type: ignore
